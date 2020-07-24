057030c0e7251572f5ad275cf86c5d6a2072875b
Tasha <tashingsworth@gmail.com>
2020-07-23 20:15:30 -0400

    created actions and reducers, set up axiosWithAuth, waiting on endpoint. Also created PrivateRoute.

---
+++ comake/src/reducers/index.js
@@ -0,0 +1,28 @@
+import * from '../actions';
+
+const initialState = {
+    issues: [],
+    singleIssue: {}
+}
+
+export const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case GET_ALL_ISSUES:
+            return {...state, issues: action.issues}
+        case GET_USER_ISSUES:
+            return {...state, issues: action.issues}
+        case GET_SPEC_ISSUE:
+            return {...state, singleIssue: action.issue}
+        case ADD_ISSUE:
+            return {...state, issues: [...state.issues, action.issue]}
+        case EDIT_ISSUE:
+            return {...state, singleIssue: action.issue}
+        case DELETE_SPEC_ISSUE:
+            return {...state, issues: state.issues.filter(issue => issue.id === action.id)}
+        case ADD_VOTE:
+            return {...state, singleIssue: {...state.singleIssue, vote: action.issue.vote + 1}, issues: state.issues.map(value => (value.id === action.id) ? {...action.issue, vote: action.issue.vote + 1} : value)}
+        case SUBTRACT_VOTE: 
+             return {...state, singleIssue: {...state.singleIssue, vote: action.issue.vote - 1}, issues: state.issues.map(value => (value.id === action.id) ? {...action.issue, vote: action.issue.vote - 1} : value)}
+        default: return {...state}
+    }
+};
