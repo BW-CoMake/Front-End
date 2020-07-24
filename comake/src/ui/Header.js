4254daa6ee0fe581df37a84e8582e578a581b37e
Tasha <tashingsworth@gmail.com>
2020-07-24 11:14:59 -0400

    set up routing for menu tabs, started drawer for menu responsiveness

--- comake/src/ui/Header.js
+++ comake/src/ui/Header.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
 
 // import logo from '../assets/logo.png';
 
 const useStyles = makeStyles(theme => ({
     toolbarMargin: {
-        ...theme.mixins.toolbar
-    }
+        ...theme.mixins.toolbar,
+        marginBottom: "3em",
+        [theme.breakpoints.down("med")]: {
+            marginBottom: "2em"
+        },
+        [theme.breakpoints.down("xs")]: {
+            marginBottom: "1.25em"
+        }
+    },
+    tabContainer: {
+        marginLeft: "auto",
+    },
+    tab: {
+        ...theme.typography.tab,
+        minWidth: 10,
+        marginLeft: "25px"
+    },
+    drawerIcon: {
+        height: "50px",
+        width: "50px"
+    },
+    drawerIconContainer: {
+        marginLeft: "auto",
+        "&:hover": {
+            backgroundColor: "transparent"
+        }
+    },
 }))
 
 function ElevationScroll(props) {
@@ -27,13 +60,79 @@ function ElevationScroll(props) {
 
 export default function Header(props) {
     const classes = useStyles();
+    const theme = useTheme();
+
+    const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+    const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+    const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
+
+    const iOS = process.browser && /iPad|iPhone|iPod/.test(navigator.userAgent);
+
+    const [openDrawer, setOpenDrawer] = useState(false);
+    const [value, setValue] = useState(0);
+
+    const handleChange = (e, newValue) => {
+        setValue(newValue);
+    };
+
+    useEffect(() => {
+        if (window.location.pathname === "/" && value !== 0) {
+            setValue(0)
+        } else if (window.location.pathname === "/about" && value !== 1) {
+            setValue(1)
+        } else if (window.location.pathname === "/login" && value !== 2) {
+            setValue(2)
+        } else if (window.location.pathname === "/register" && value !== 3) {
+            setValue(3)
+        }
+    }, [value]);
+
+    const tabs = (
+        <>
+            <Tabs
+                value={value}
+                onChange={handleChange} className={classes.tabContainer}
+                indicatorColor="primary">
+                <Tab className={classes.tab}
+                    component={Link}
+                    to="/"
+                    label="Home" />
+                <Tab className={classes.tab}
+                    component={Link}
+                    to="/about"
+                    label="About" />
+                <Tab className={classes.tab}
+                    component={Link}
+                    to="/login"
+                    label="Login" />
+                <Tab className={classes.tab}
+                    component={Link}
+                    to="/register"
+                    label="Sign Up" />
+            </Tabs>
+        </>
+    );
+
+    const drawer = (
+        <>
+            <SwipeableDrawer disableBackdropTransition={!iOS} disableDiscovery={iOS} open={openDrawer} onClose={() => setOpenDrawer(false)} onOpen={() => setOpenDrawer(true)}>
+                Example Drawer
+            </SwipeableDrawer>
+            <IconButton 
+            className={classes.drawerIconContainer}
+            onClick={() => setOpenDrawer(!openDrawer)} disableRipple>
+                <MenuIcon className={classes.drawerIcon}/>
+            </IconButton>
+        </>
+    )
 
     return (
         <>
             <ElevationScroll>
                 <AppBar color="primary">
-                    <Toolbar>
+                    <Toolbar disableGutters>
                         {/* <img src={logo} alt="company logo" /> */}
+                        {matchesMD ? drawer : tabs}
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
